3b3813031eabb35b865a7c4e7aa591a5d7c1bc02
SohiniRP <SohiniPaul@rightpoint.com>
2025-12-23 12:45:15 +0530

    Updated ModificationDuringIteration assignment

--- src/com/week1/assignment/collection/ModificationDuringIteration.java
+++ src/com/week1/assignment/collection/ModificationDuringIteration.java
@@ -1,6 +1,8 @@
 package com.week1.assignment.collection;
 
+import javax.swing.text.html.HTMLDocument;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.concurrent.CopyOnWriteArrayList;
 
 public class ModificationDuringIteration {
@@ -13,21 +15,30 @@ public class ModificationDuringIteration {
         list.add("Banana");
         list.add("Mango");
 
-        for (String s : list) {
-//            list.remove(s); // Throws ConcurrentModificationException
-            System.out.println(s);
-        }
+//        for (String s : list) {
+////            list.remove(s); // Throws ConcurrentModificationException
+//            System.out.println(s);
+//        }
+//
+//        //CopyOnWriteArrayList allows modification while iterating and does NOT throw ConcurrentModificationException
+//        CopyOnWriteArrayList<String> modifiableList = new CopyOnWriteArrayList<>();
+//
+//        modifiableList.add("Apple");
+//        modifiableList.add("Banana");
+//        modifiableList.add("Mango");
+//
+//        for (String s : modifiableList) {
+//            modifiableList.remove(s); // Throws ConcurrentModificationException
+//            System.out.println(s);
+//        }
 
-        //CopyOnWriteArrayList allows modification while iterating and does NOT throw ConcurrentModificationException
-        CopyOnWriteArrayList<String> modifiableList = new CopyOnWriteArrayList<>();
+        //Using Iterator
 
-        modifiableList.add("Apple");
-        modifiableList.add("Banana");
-        modifiableList.add("Mango");
+        Iterator<String> iterator = list.iterator();
 
-        for (String s : modifiableList) {
-            modifiableList.remove(s); // Throws ConcurrentModificationException
-            System.out.println(s);
+        while(iterator.hasNext()){
+            if(iterator.next().equals("Banana")) iterator.remove();
+            System.out.println(list);
         }
     }
 }
